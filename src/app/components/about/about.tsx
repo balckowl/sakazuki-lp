89f3be098201912a50219a4e791438a56e7a3aef
K.A <>
2024-03-23 14:06:12 +0900

    create: adapterとaboutを作成しました。

---
+++ src/app/components/about/about.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='container h-[500px]'>
+            <div className='flex items-center justify-center h-full'>
+                <div className="w-[70%]">
+                    <h3 className='text-[40px] font-bold mb-4'>キャリアの流れを見える化し、<br />後継へつなぐ</h3>
+                    <p>
+                        sakazukiは、「キャリア証明書」の発行によって個人の成長を促進し、彼らの人材市場における評価を通じて企業認知度を向上させる採用支援サービスです。
+                        あらゆる企業を、求職者や未来のパートナーから選ばれる人材輩出企業へと導くことが、私たちのミッションです。
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ src/app/components/hero/adapter.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Adapter = () => {
+    return (
+        <div className='container py-[40px]'>
+            <div className='flex items-center justify-around h-full'>
+                <div>
+                    <img src="/images/hero/musasino.jpeg" alt="" />
+                </div>
+                <div>
+                    <img src="/images/hero/chibakougyou.jpg" alt="" className='w-[150px]' />
+                </div>
+                <div>
+                    <img src="/images/hero/dotshapy.jpeg" alt="" className='w-[150px]' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Adapter
